1178d24b66d5e1309f366d20b5cfe8a8c5a97ce0
Christian Nagel <christian@christiannagel.com>
2019-06-18 13:16:37 +0200

    authentication with owin

---
+++ aspnet/AuthenticationWithOwin/AuthenticationWithOwin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthenticationWithOwin.Startup))]
+namespace AuthenticationWithOwin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
